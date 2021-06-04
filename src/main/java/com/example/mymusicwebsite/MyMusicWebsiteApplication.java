907eb79ce5be0cdecb7b0ebe5491cbb3bd4f80f2
yli394 <yanchun.li@mail.mcgill.ca>
2021-06-04 11:07:50 -0400

    update

--- src/main/java/com/example/mymusicwebsite/MyMusicWebsiteApplication.java
+++ src/main/java/com/example/mymusicwebsite/MyMusicWebsiteApplication.java
@@ -3,6 +3,13 @@ package com.example.mymusicwebsite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+Hot update and hot reload:
+1. Ctrl + Shift + A --> search Registry..., find the one with 3 dots.
+   Then find compiler.automake.allow.when.running, select it.
+2. Ctrl + F9 for hot reload
+ */
+
 @SpringBootApplication
 public class MyMusicWebsiteApplication {
 
